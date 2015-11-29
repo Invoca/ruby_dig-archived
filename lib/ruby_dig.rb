5a42c4163b792acfbc2e1f6523bfadeb71d37b34
Colin Kelley <colindkelley@gmail.com>
2015-11-28 23:25:01 -0800

    Refactor

--- lib/ruby_dig.rb
+++ lib/ruby_dig.rb
@@ -1,13 +1,11 @@
-require "ruby_dig/version"
-
 module RubyDig
   def dig(key, *other_keys)
     if value = self[key] rescue nil
-      if other_keys.any?
-        other_key, *rest = other_keys
-        value.dig(other_key, *rest) if value.is_a?(RubyDig)
-      else
+      if other_keys.empty?
         value
+      elsif value.is_a?(RubyDig)
+        other_key, *rest = other_keys
+        value.dig(other_key, *rest)
       end
     end
   end
